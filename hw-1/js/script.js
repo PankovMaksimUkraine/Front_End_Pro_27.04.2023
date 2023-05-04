daca44f8ae1148cf19c552d6b2fe27121c31b02b
Maksym <pankovmaksim@ukr.net>
2023-05-04 12:12:01 +0300

    add hw-2

---
+++ hw-1/js/script.js
@@ -0,0 +1,16 @@
+const command = prompt('Какое действие ты хочешь выполнить?');
+const num1 = +prompt('Какое первое число?');
+const num2 = +prompt('Какое второе число?');
+if(command === "add"){
+    alert( `Получается ${num1 + num2} все верно?`)     
+}
+if(command === "sub"){
+    alert( `Получается ${num1 - num2} все верно?`)     
+}
+if(command === "mult"){
+    alert( `Получается ${num1 * num2} все верно?`)     
+}
+if(command === "div"){
+   alert( `Получается ${num1 / num2} все верно?`)    
+}
+

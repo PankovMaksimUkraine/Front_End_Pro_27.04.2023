10eb604f85b7f3baa2710707d8356b73d41da3eb
Maksym <pankovmaksim@ukr.net>
2023-05-04 12:30:15 +0300

    last version

--- hw-2/js/script.js
+++ hw-2/js/script.js
@@ -2,4 +2,4 @@ const num1 = +prompt('Какое первое число?');
 const num2 = +prompt('Какое второе число?');
 const num3 = +prompt('Какое третье число?');
 const result =  (num1 + num2 + num3)/3 ;
-alert(`Смотри,это среднее орифметическое (${num1} + ${num2} + ${num3})/3 =  ${result}! Все верно?`)
+alert(`Смотри,это среднее арифметическое (${num1} + ${num2} + ${num3})/3 =  ${result}! Все верно?`)

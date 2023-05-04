599b4aa73a482c68961833192d07c49f8cdb14b1
Maksym <pankovmaksim@ukr.net>
2023-05-04 12:22:24 +0300

    fixed

--- hw-1/js/script.js
+++ hw-1/js/script.js
@@ -2,16 +2,32 @@ const command = prompt('Какое действие ты хочешь выпол
 const num1 = +prompt('Какое первое число?');
 const num2 = +prompt('Какое второе число?');
 if(command === "add"){
-    alert( `Получается ${num1 + num2} все верно?`)     
+    alert(` ${num1} + ${num2} = ${num1+num2}`)     
 }
 if(command === "sub"){
-    alert( `Получается ${num1 - num2} все верно?`)     
+    alert(` ${num1} - ${num2} = ${num1-num2}`)        
 }
 if(command === "mult"){
-    alert( `Получается ${num1 * num2} все верно?`)     
+    alert(` ${num1} * ${num2} = ${num1*num2}`)        
 }
 if(command === "div"){
-   alert( `Получается ${num1 / num2} все верно?`)    
+    alert(` ${num1} / ${num2} = ${num1/num2}`)     
 }
 
+
+// const command = prompt('Какое действие ты хочешь выполнить?');
+// const num1 = +prompt('Какое первое число?');
+// const num2 = +prompt('Какое второе число?');
+// if(command === "add"){
+//     alert( `Получается ${num1 + num2} все верно?`)     
+// }
+// if(command === "sub"){
+//     alert( `Получается ${num1 - num2} все верно?`)     
+// }
+// if(command === "mult"){
+//     alert( `Получается ${num1 * num2} все верно?`)     
+// }
+// if(command === "div"){
+//    alert( `Получается ${num1 / num2} все верно?`)    
+// }
 

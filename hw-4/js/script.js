572b4ea077cccb309b344fed2f57b7e737628e02
Maksym <pankovmaksim@ukr.net>
2023-05-10 12:25:53 +0300

    first

---
+++ hw-4/js/script.js
@@ -0,0 +1,11 @@
+// 1 завдання
+
+for(let num = 20 ; num <= 30; num += 0.5) {
+     console.log(num)
+}
+
+// 2 завдання
+
+for(let sum = 10 ; sum <= 100;sum += 10) {
+     console.log(`${sum} доларів = ${sum*27}`)
+}

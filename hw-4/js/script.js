966b38038adb73372b0c481bdf8de434a92fe77f
Maksym <pankovmaksim@ukr.net>
2023-05-11 13:42:14 +0300

    fix

--- hw-4/js/script.js
+++ hw-4/js/script.js
@@ -1,13 +1,13 @@
 // 1 завдання
 
 for(let num = 20 ; num <= 30; num += 0.5) {
-     console.log(num)
+     console.log(num);
 }
 
 // 2 завдання
 
 for(let sum = 10 ; sum <= 100;sum += 10) {
-     console.log(`${sum} доларів = ${sum*27}`)
+     console.log(`${sum} доларів = ${sum*27}`);
 }
 
 // 3 завдання
@@ -16,7 +16,7 @@ for(let sum = 10 ; sum <= 100;sum += 10) {
 let n= 25 ;
 for(let num1 = 1 ; num1 <= 100; num1++) {
      if(num1**2<= n) {
-          console.log(num1)
+          console.log(num1);
      }
 }
 
@@ -26,7 +26,7 @@ const number = 7;
 let result = true;
 
 if (number === 1) {
-    console.log("1 не є ні простим, ні складеним числом.");
+    console.log("Число 1 - не є ні простим, ні складовим числом, тому що у нього лише один дільник - 1.");
 }
 else if (number > 1) {
     for (let i = 2; i < number; i++) {
@@ -38,7 +38,7 @@ else if (number > 1) {
     if (result) {
         console.log(`${number}  просте число`);
     } else {
-        console.log(`${number} є не простим числом`);
+        console.log(`${number} НЕ  є простим числом`);
     }
 }
 
@@ -49,12 +49,12 @@ let o = 81 ;
 let result2 = false;
 for(let degree = 1 ; degree <= 100; degree++) {
      if( o == 3**degree ){
-        result2 = true
+        result2 = true;
      }
 }
 if (result2) {
-    console.log( `число ${o} можна одержати це число шляхом зведення числа 3 у деякий ступінь `)
+    console.log( `число ${o} можна одержати  шляхом зведення числа 3 у деякий ступінь `);
 } else {
-    console.log( `число ${o}   не можна одержати це число шляхом зведення числа 3 у деякий ступінь `)
+    console.log( `число ${o} НЕ можна одержати  шляхом зведення числа 3 у деякий ступінь `);
 }
 
